9ebf350a816a0a34e8f66dd49c8d678535e449b1
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-09-07 19:33:02 +0200

    all the flags possible

--- smol.c
+++ smol.c
@@ -1,16 +1,45 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  // ICRNL - input flag carriage return newline
+  // IXON turns off software control flow
+  // BRKINT - for break condition
+  // INPCK - enables parity checking
+  // INSTRIP - causes 8th but of each input byte to be stripped - probably
+  // already turned off
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  // OPOST turns off all output processing features
+  raw.c_lflag &= ~(OPOST);
+  // CS8 - NOT A FLAG - its bit mask
+  raw.c_cflag |= (CS8);
+  // ICONON turns off canonical mode
+  // ISIG does it for SIGINT SIGTSTP;
+  // IEXTEN does it for ctrl-v;
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  };
   return 0;
 }
