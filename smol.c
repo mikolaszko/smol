6ae0f35e442b60f8f421fba5ed26560b6c379a57
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-09-08 13:15:26 +0200

    add error handling

--- smol.c
+++ smol.c
@@ -1,14 +1,29 @@
+// includes
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// data
 struct termios orig_termios;
 
-void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+// terminal
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
 
 void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
+
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
@@ -28,18 +43,27 @@ void enableRawMode() {
   // ISIG does it for SIGINT SIGTSTP;
   // IEXTEN does it for ctrl-v;
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
 }
+
+// init
 int main() {
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q')
+      break;
   };
   return 0;
 }
