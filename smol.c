6ad04d228e889400e6950757b4ccf1d5d29881cd
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-09-09 14:57:33 +0200

    impl of ab buffer

--- smol.c
+++ smol.c
@@ -1,15 +1,14 @@
 // includes
-#include <ctype.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 // defines
-//
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 // data
@@ -109,6 +108,28 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+// append buffer
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT                                                              \
+  { NULL, 0 }
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL)
+    return;
+
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) { free(ab->b); }
+
 // commands
 void editorProcessCommand(char c) {
   // :q command for now quiting
@@ -122,25 +143,33 @@ void editorProcessCommand(char c) {
 }
 
 // output
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~", 1);
+    abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 3);
+      abAppend(ab, "\r\n", 2);
     }
   }
 }
+// accumulate all of the tildres and escape chars into buf and then write to it
 void editorRefreshScreen() {
-  // clear entire screen
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  // move cursor to 0x0 (row-column)
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  // hide cursor
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[H", 3);
+  // hide cursor
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 // input
